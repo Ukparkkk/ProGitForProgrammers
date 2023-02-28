31c0a8b7c7d847b769ef974a30379ae3aee44c33
$USER <sanguk@thingshub.kr>
2023-02-28 16:31:54 +0900

    "Program.cs 추가"

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Stytem;
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
